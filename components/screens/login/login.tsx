167e6b043c2fca11285f635fdb7aa7972c5bd7b8
root <tiago.wy@outlook.com>
2022-05-18 22:10:36 -0300

    Commit 18

--- components/screens/login/login.tsx
+++ components/screens/login/login.tsx
@@ -1,17 +1,51 @@
-import * as React from "react";
-import { View, Text } from 'react-native';
+import React, {useState} from "react";
+import { View, SafeAreaView, TextInput, StyleSheet, Dimensions } from 'react-native';
 
 import {OlhoExterno} from './styles'
 
 function Login() {
 
+  const [value, setValue] = useState('');
+  var width = Dimensions.get('window').width;
+
   return (
 
-    <View>
-        <OlhoExterno />
+    <View style={styles.container}>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={setValue}
+          value={value}
+        />
+        <TextInput
+          style={styles.input}
+          // onChangeText={onChangeNumber}
+          value={value}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        />
+      </SafeAreaView>
     </View>
 
   );
 }
 
 export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#FF6666',
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20
+  },
+  input: {
+    height: 10,
+    margin: 12,
+    flex: 2,
+
+    borderWidth: 1,
+    padding: 10,
+  },
+});
